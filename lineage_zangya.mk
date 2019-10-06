5db2a37a9d7601f1d9b83b6b9b069a2481855065
Eloi Carracedo <eloimuns@gmail.com>
2019-10-06 13:30:17 +0200

    zangya: Update device name and product name
    Change-Id: Iaf110c671c3f328699229dae3b072014805d9ad8

--- lineage_zangya.mk
+++ lineage_zangya.mk
@@ -20,7 +20,8 @@ PRODUCT_MANUFACTURER := bq
 PRODUCT_RELEASE_NAME := zangya
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRODUCT_NAME=zangya \
-        PRIVATE_BUILD_DESC="zangya_bq-user 9 PKQ1.190118.001 919 release-keys"
+        PRIVATE_BUILD_DESC="zangya_bq-user 9 PKQ1.190118.001 919 release-keys" \
+        PRODUCT_NAME="zangya_bq" \
+        TARGET_DEVICE="zangya_sprout"
 
 BUILD_FINGERPRINT := bq/zangya_bq/zangya_sprout:9/PKQ1.190118.001/919:user/release-keys
